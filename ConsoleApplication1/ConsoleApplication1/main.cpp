954fa07e11ec3ab8e01a63cf894560d4a89e555f
Conor <cjohns201@caledonian.ac.uk>
2014-09-30 09:44:25 +0100

    first save before properties were addapted

---
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -0,0 +1,23 @@
+//Header files 
+#include <iostream>
+
+//Global variables go here
+
+
+
+
+
+//Global functions 
+void InitWindow(int width, int height, bool fullscreen) {
+
+
+}
+
+void CleanUp() {
+}
+
+//Main Method - Entry Point 
+int main(int argc, char * arg[]) {
+	return 0;
+}
+
