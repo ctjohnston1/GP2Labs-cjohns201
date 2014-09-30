e1b7077570ebb1edb406eacaacff58b6c605e187
Conor <cjohns201@caledonian.ac.uk>
2014-09-30 10:51:46 +0100

    up to point 7

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,23 +1,72 @@
 //Header files 
 #include <iostream>
+//header for SDL2 functionality
+#include <SDL.h>
+
 
 //Global variables go here
 
+//Pointer to our SDL Windows
+SDL_Window * window;
 
+//Constants to control window creation
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+bool running = true;
 
 
 
 //Global functions 
 void InitWindow(int width, int height, bool fullscreen) {
+	//Create a window
+	window = SDL_CreateWindow(
+		"Lab 1",             // window title
+		SDL_WINDOWPOS_CENTERED,     // x position, centered
+		SDL_WINDOWPOS_CENTERED,     // y position, centered
+		width,                        // width, in pixels
+		height,                        // height, in pixels
+		SDL_WINDOW_OPENGL           // flags
+		);
 
 
 }
 
+
+
 void CleanUp() {
+SDL_DestroyWindow(window);
+SDL_Quit();
 }
 
 //Main Method - Entry Point 
 int main(int argc, char * arg[]) {
+
+	// init everything - SDL, if it is nonzero we have a problem
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "ERROR SDL_Init " << SDL_GetError() << std::endl;
+
+		return -1;
+	}
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+
+	SDL_Event event;
+	while (running)
+	{
+		while (SDL_PollEvent(&event)) {
+			//Get event type
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				//set our boolean which controls the game loop to false
+				running = false;
+			}
+
+		}
+
+	}
+
+
+
+	CleanUp();
 	return 0;
 }
 
