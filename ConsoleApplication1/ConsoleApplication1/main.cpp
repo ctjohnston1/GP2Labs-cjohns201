9c1a5ad837dac6c671e0b8b25889e2bacca3e7f9
Conor Johnston <cjohns201@caledonian.ac.uk>
2014-10-14 10:15:02 +0100

    Lab 2 Completed

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,12 +1,13 @@
 //Header files 
 #include <iostream>
+
 //header for SDL2 functionality
 #include <GL/glew.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <gl\GLU.h>
 
-
+#include "Vertex.h"
 
 //Global variables go here
 //SDL GL Context
@@ -14,15 +15,60 @@ SDL_GLContext glcontext = NULL;
 GLuint triangleVBO;
 //Pointer to our SDL Windows
 SDL_Window * window;
+GLuint triangleEBO;
+
 
 //Constants to control window creation
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 bool running = true;
 
-float triangleData[] = { 0.0f, 1.0f, 0.0f, // Top
--1.0f, -1.0f, 0.0f, // Bottom Left
-1.0f, -1.0f, 0.0f }; //Bottom Right
+																					
+
+
+Vertex triangleData[] = { //Front
+	//Front
+	{ -0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f },// Top Left
+	{ -0.5f, -0.5f, 0.5f,
+	1.0f, 1.0f, 0.0f, 1.0f },// Bottom Left
+	{ 0.5f, -0.5f, 0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+	{ 0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f },// Top Right
+	//back
+	{ -0.5f, 0.5f, -0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f },// Top Left
+	{ -0.5f, -0.5f, -0.5f,
+	1.0f, 1.0f, 0.0f, 1.0f },// Bottom Left
+	{ 0.5f, -0.5f, -0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+	{ 0.5f, 0.5f, -0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f },// Top Right
+
+}; //r,g,b,a
+
+
+GLuint indices[] = {
+	//front
+	0, 1, 2,
+	0, 3, 2,
+	//left
+	4, 5, 1,
+	4, 1, 0,
+	//right
+	3, 7, 2,
+	7, 6, 2,
+	//bottom
+	1, 5, 2,
+	6, 2, 1,
+	//top
+	5, 0, 7,
+	5, 7, 3,
+	//back
+	4, 5, 6,
+	4, 7, 6
+};
 
 //Global functions 
 void InitWindow(int width, int height, bool fullscreen) {
@@ -46,6 +92,7 @@ glDeleteBuffers(1, &triangleVBO);
 SDL_DestroyWindow(window);
 SDL_Quit();
 SDL_GL_DeleteContext(glcontext);
+glDeleteBuffers(1, &triangleEBO);
 }
 
 
@@ -129,6 +176,16 @@ void initGeometry()
 																		//Copy Vertex Data to VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleData),
 		triangleData, GL_STATIC_DRAW);
+
+	//new buffer EBO
+
+	//create buffer
+	glGenBuffers(1, &triangleEBO);
+	//Make the EBO active
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangleEBO);
+	//Copy Index data to the EBO
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+		GL_STATIC_DRAW);
 }
 
 //Function to draw
@@ -143,6 +200,7 @@ void render()
 																		//Make the new VBO active. Repeat here as a sanity check( may have changed
 																		//since initialisation)
 	glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangleEBO);
 																		//Establish its 3 coordinates per vertex with zero stride(space between elements) 
 																		//in array and contain floating point numbers
 	glVertexPointer(3, GL_FLOAT, 0, NULL);
@@ -163,30 +221,44 @@ void render()
 																		//Translate to -5.0f on z-axis
 	glTranslatef(0.0f, 0.0f, -5.0f);
 																		//Begin drawing triangles
-/*	glBegin(GL_TRIANGLES);
-	glColor3f(1.0f, 0.0f, 0.0f); //Colour of the vertices
-	glVertex3f(0.0f, 1.0f, 0.0f); // Top
-	glVertex3f(-1.5f, -1.5f, 0.0f); // Bottom Left
-	glVertex3f(1.5f, -1.5f, 0.0f); // Bottom Right
-	glEnd();
+			/*	glBegin(GL_TRIANGLES);
+				glColor3f(1.0f, 0.0f, 0.0f); //Colour of the vertices
+				glVertex3f(0.0f, 1.0f, 0.0f); // Top
+				glVertex3f(-1.5f, -1.5f, 0.0f); // Bottom Left
+				glVertex3f(1.5f, -1.5f, 0.0f); // Bottom Right
+				glEnd();
 
-	glBegin(GL_TRIANGLES);
-	glColor3f(15.0f, 33.0f, 8.0f); //Colour of the vertices
-	glVertex3f(0.0f, 1.0f, 0.0f); // Top
-	glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-	glVertex3f(1.0f, -1.0f, 0.0f); // Bottom Right
-	glEnd();
-*/
+				glBegin(GL_TRIANGLES);
+				glColor3f(15.0f, 33.0f, 8.0f); //Colour of the vertices
+				glVertex3f(0.0f, 1.0f, 0.0f); // Top
+				glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+				glVertex3f(1.0f, -1.0f, 0.0f); // Bottom Right
+				glEnd();
+				*/
 																		//Swith to ModelView
 	glMatrixMode(GL_MODELVIEW);
 																		//Reset using the Indentity Matrix
 	glLoadIdentity();
-																		//translate
+	gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0f, 0.0, 1.0, 0.0);																	//translate
 	glTranslatef(0.0f, 0.0f, -6.0f);
 																		//Actually draw the triangle, giving the number of vertices provided
-	glDrawArrays(GL_TRIANGLES, 0, sizeof(triangleData) / (3 * sizeof(float)));
+	//glDrawArrays(GL_TRIANGLES, 0, sizeof(triangleData) / (3 * sizeof(Vertex)));
 
-																		//require to swap the back and front buffer
+																		
+
+
+	//the 3 parameter is now filled out, the pipeline needs to know the size of
+	//each vertex
+	glVertexPointer(3, GL_FLOAT, sizeof(Vertex), NULL);
+	//The last parameter basically says that the colours start 3 floats into
+	// each element of the array
+	glColorPointer(4, GL_FLOAT, sizeof(Vertex), (void**)(3 * sizeof(float)));
+	//Establish array contains vertices & colours
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint),
+		GL_UNSIGNED_INT, 0);
+//require to swap the back and front buffer
 	SDL_GL_SwapWindow(window);
 
 
