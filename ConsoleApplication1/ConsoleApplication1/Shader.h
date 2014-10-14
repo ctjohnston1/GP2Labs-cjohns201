fba3b869ac3634b61b8895390966b9a144128afb
Conor Johnston <cjohns201@caledonian.ac.uk>
2014-10-14 10:58:01 +0100

    Lab 3 - section 2.3
    writing in the code needed for shader.cpp

---
+++ ConsoleApplication1/ConsoleApplication1/Shader.h
@@ -0,0 +1,20 @@
+#ifndef Shader_h
+#define Shader_h
+
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+enum SHADER_TYPE
+{
+	VERTEX_SHADER = GL_VERTEX_SHADER,
+	FRAGMENT_SHADER = GL_FRAGMENT_SHADER
+};
+
+GLuint loadShaderFromFile(const std::string& filename, SHADER_TYPE shaderType);
+GLuint loadShaderFromMemory(const char * pMem, SHADER_TYPE shaderType);
+
+
+#endif
