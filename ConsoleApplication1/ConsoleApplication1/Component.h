9d89636ef991184deeaf8832ee87f0d967668201
Conor Johnston <cjohns201@caledonian.ac.uk>
2014-10-28 10:51:39 +0000

    lab six at the start of point 2

---
+++ ConsoleApplication1/ConsoleApplication1/Component.h
@@ -0,0 +1,26 @@
+#ifndef Component_h
+#define Component_h
+#include <string>
+
+class Component{
+
+public:
+	Component();
+	virtual~Component();
+	virtual void init();
+	virtual void update();
+	virtual void render();
+	virtual void destroy();
+    const std::string& getType();
+	bool toggleActive();
+	bool isActive();
+
+protected:
+	std::string m_Type;
+	bool m_Active;
+		 
+private:
+
+};
+
+#endif 
